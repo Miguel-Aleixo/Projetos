582ca216ceb47c6c7562faac2a8b4282d38ac9ef
Miguel_Aleixo <migueloliveiraaleixosantos1@gmail.com>
2025-02-23 13:59:12 -0300

    Finalizado

--- Calculadoras/Calculadora 2/3.js
+++ Calculadoras/Calculadora 2/3.js
@@ -1,1 +1,18 @@
-jxnjxj
+function inserir(num) {
+     let numero = document.getElementById('resultado').innerHTML;
+     document.getElementById('resultado').innerHTML = numero + num;
+}
+
+function clean() {
+    document.getElementById('resultado').innerHTML = "";
+}
+
+function apagar() {
+    let apagar = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = apagar.substring(0, apagar.length - 1);
+}
+
+function calcular() {
+    let calcular = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = eval(calcular)
+}
